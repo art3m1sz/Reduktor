f5c0428e6f102a2ab86656d097f6a156827ba4df
Aby <127810005+art3m1sz@users.noreply.github.com>
2024-07-06 00:11:43 +0800

    Update Banner.tsx

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -5,7 +5,7 @@ export default function Banner (){
     return (
         <Flex zIndex={1} direction={"column"} align={"center"}>
                     <Flex w={"200px"} h={"200px"}>
-                    <Image src="/Reduktor-logo-dark2.png" alt="banner" width={1000} height={1000}></Image>
+                    <Image src="/Reduktor-logo-dark2.PNG" alt="banner" width={1000} height={1000}></Image>
                     </Flex>
                     <Flex direction={"column"} mt={"48px"} align={"center"}>
                       <Text color={"white"} fontSize={"64px"} fontWeight={"bold"}>WELCOME TO REDUKTOR</Text>
